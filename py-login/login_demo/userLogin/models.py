bf931363bd6f932a29fef1fd5ce6439c9f9c9618
GoShunhou <w62442@gmail.com>
2025-05-30 11:02:03 +0900

    結合検索の条件追加

--- py-login/login_demo/userLogin/models.py
+++ py-login/login_demo/userLogin/models.py
@@ -6,3 +6,12 @@ class UserInfo(models.Model):
     password = models.CharField(verbose_name="パスワード",max_length=32)
     age = models.IntegerField(verbose_name="年齢")
     mobileNumber = models.CharField(verbose_name="年齢",max_length=11)
+
+class Department(models.Model):
+    title = models.CharField(verbose_name="タイトル",max_length=32)
+
+class User(models.Model):
+    name = models.CharField(verbose_name="名前",max_length=12)
+    age = models.IntegerField(verbose_name="年齢")
+    salary = models.IntegerField(verbose_name="給料")
+    depart = models.ForeignKey(verbose_name="部署",to="Department",on_delete=models.CASCADE)
